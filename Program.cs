2497dd2314d036691ec0cebc70fc269ca4b63edd
Nadya <nadya0707@yandex.ru>
2023-02-05 02:25:52 +0300

    commit 1

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿//Задайте одномерный массив, заполненный случайными числами. 
+//Найдите сумму элементов, стоящих на нечётных позициях.
+
+void Input_array (int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    array [i] = new Random().Next(100,1000);
+}
+int release(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i+=2)
+        sum = sum + array [i];
+    return sum;
+}
+Console.Write("Введите колличество элементов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int [] array =new int [n];
+Input_array(array);
+Console.WriteLine($"[{string.Join(" , " , array)}]");
+Console.WriteLine(release(array));
